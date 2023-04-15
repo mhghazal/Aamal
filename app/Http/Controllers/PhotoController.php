c9d3a93b52cadd8392e5a83a97db6bebff26f44b
mohammad-gh <mohammadghazal075@gmail.com>
2023-04-15 17:27:42 +0300

    test

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -14,38 +14,7 @@ class PhotoController extends Basecontroller
     public function image(Request $request)
     {
     }
-    public function show($id)
+    public function show()
     {
-        $photo = Photo::findOrFail($id);
-
-        return response($photo->photo)->header('Content-Type', 'image/jpeg');
-    }
-
-    public function store(Request $request)
-    {
-        $photo = new Photo;
-        $photo->name = $request->name;
-        $photo->photo = $request->file('photo')->get();
-        // $photo->photo = $photoData;
-        $photo->save();
-    }
-
-    public function test()
-    {
-        $array = [
-            ['user' => ['id' => 1, 'name' => 'User 1', 'email' => 'user1@example.com']],
-            ['user' => ['id' => 2, 'name' => 'User 2', 'email' => 'user2@example.com']],
-        ];
-
-        $emails = [];
-
-        return Arr::pluck($array, 'user');
-        //  $emails;
-    }
-
-    public  function tt(AuthManager $auth)
-    {
-        $user = $auth->user();
-        $user = Auth::user();
     }
 }
